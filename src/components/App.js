d26b692f7ad74c4bc1b79fe005e6a02936b77516
Yash Prakash Mishra <34412462+xcalibre1@users.noreply.github.com>
2021-02-06 01:27:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,40 +2,45 @@ import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: { left: "0px" }
     };
+    this.renderChoice = this.renderBallOrButton.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+  }
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-		}
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+  }
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else {
+      return (
+        <button onClick={this.buttonClickHandler}>Click For One Ball</button>
+      );
     }
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
+  // bind ArrowRight keydown event
+  componentDidMount() {
+    document.addEventListener("keydown", (event) => {
+      let copyobj = { ...this.state.ballPosition };
+      if (event.keyCode === 39) {
+        this.setState({ posi: this.state.posi + 5 });
+      }
+      copyobj.left = this.state.posi + "px";
+      this.setState({ ballPosition: copyobj });
+    });
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
+  render() {
+    return <div className="playground">{this.renderBallOrButton()}</div>;
+  }
 }
 
 
